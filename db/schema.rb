0562dbb9c6cd90ed0978cf685c0535a24283b4ce
keisho777 <komatsu.kei0130@gmail.com>
2026-01-12 02:20:51 +0900

    feat: workテーブルを作成
    figureテーブルの参照先として使用するため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_11_165341) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_11_171307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,5 +35,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_11_165341) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end

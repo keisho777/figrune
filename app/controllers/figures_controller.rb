9a769c4ee3f5ddfd61f5187e14a49f41d3a12419
keisho777 <komatsu.kei0130@gmail.com>
2026-01-13 18:42:56 +0900

    feat: ヘッダーの「登録」にリンクを設定
    登録画面に行くリンクを設定していなかったため。（本時点での登録画面は導線確認のための仮登録画面です）

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,22 @@
 class FiguresController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @q = current_user.figures.ransack(params[:q])
     @figures = @q.result(distinct: true).order("created_at desc")
+  end
+
+  def new
+    @figure = Figure.new
+  end
+
+  def create
+    @figure = current_user.figures.build(figure_params)
+    if @figure.save
+      redirect_to figures_path, success: t('defaults.flash_message.created')
+    else
+      # flash.now[:danger] = t('defaults.flash_message.not_created')
+      render :new, status: :unprocessable_entity
+    end
   end
 end

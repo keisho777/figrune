40f24860993b64c13e0ddcec8377c8e7ce6f0f48
keisho777 <komatsu.kei0130@gmail.com>
2026-01-13 15:27:54 +0900

    fix: indexアクションのデータ取得処理を修正
    他ユーザーの登録済みフィギュアを見れるようになっていたため

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,7 @@
 class FiguresController < ApplicationController
   before_action :authenticate_user!
   def index
-    @q = Figure.ransack(params[:q])
-    @figures = @q.result(distinct: true).includes(:user).order("created_at desc")
+    @q = current_user.figures.ransack(params[:q])
+    @figures = @q.result(distinct: true).order("created_at desc")
   end
 end

a540210ea73802b6642d0e367a37018b7d612e21
keisho777 <komatsu.kei0130@gmail.com>
2026-01-16 00:39:29 +0900

    feat: フィギュア登録機能の実装
    フィギュアの管理をするには登録機能が必須なため

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,15 +8,28 @@ class FiguresController < ApplicationController
 
   def new
     @figure = Figure.new
+    # 初期値設定のため
+    @figure.quantity = 1
   end
 
   def create
-    @figure = current_user.figures.build(figure_params)
+     @figure = current_user.figures.build(figure_params)
+     # 以下3点のassign_テーブル名_by_nameについて
+     # フォームで入力された名称をもとに、各関連モデルを取得（なければ作成）して Figure に紐付ける
+     @figure.assign_work_by_name(@figure.work_name)
+     @figure.assign_shop_by_name(@figure.shop_name)
+     @figure.assign_manufacture_by_name(@figure.manufacture_name)
     if @figure.save
-      redirect_to figures_path, success: t('defaults.flash_message.created')
+      redirect_to figures_path, success: t("defaults.flash_message.created")
     else
-      # flash.now[:danger] = t('defaults.flash_message.not_created')
+      flash.now[:danger] = t("defaults.flash_message.not_created")
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def figure_params
+    params.require(:figure).permit(:name, :release_month, :quantity, :price, :payment_status, :size_type, :size_mm, :work_name, :shop_name, :manufacture_name, :note)
   end
 end

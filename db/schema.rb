45251f88d1e6f13b2d2a6cd0ca40838c433e5a8e
keisho777 <komatsu.kei0130@gmail.com>
2026-01-17 13:55:50 +0900

    refactor: manufacturesテーブルをmanufacturersテーブルに修正
    スペルミスのため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_11_172820) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_17_044048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_11_172820) do
     t.index ["work_id"], name: "index_figures_on_work_id"
   end
 
-  create_table "manufactures", force: :cascade do |t|
+  create_table "manufacturers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_11_172820) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "figures", "manufactures"
+  add_foreign_key "figures", "manufacturers", column: "manufacture_id"
   add_foreign_key "figures", "shops"
   add_foreign_key "figures", "users"
   add_foreign_key "figures", "works"

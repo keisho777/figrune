4f2246f5c0a284385c1629a689b0d8647c732974
keisho777 <komatsu.kei0130@gmail.com>
2026-01-22 17:57:33 +0900

    chore: production.rbにrenderの仮URLをconfig.hostsに追加
    ドメインが有効になるまで時間がかかるため、確認ができるまではonrender.comでも接続可能とする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts << "figrune-app.com"
   config.hosts << "www.figrune-app.com"
+  config.hosts << "figrune.onrender.com"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end

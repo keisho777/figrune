54fa307f5c1cf39b69d336e555e08130cc39b0d4
keisho777 <komatsu.kei0130@gmail.com>
2026-01-25 12:59:45 +0900

    feat: パスワード変更機能の実装
    ユーザーが個人でパスワードを変更・管理できるようにするため

--- app/controllers/account_settings_controller.rb
+++ app/controllers/account_settings_controller.rb
@@ -7,7 +7,7 @@ class AccountSettingsController < ApplicationController
   end
 
   def edit_password
-    
+    @user = current_user
   end
 
   def update_email
@@ -20,9 +20,34 @@ class AccountSettingsController < ApplicationController
     end
   end
 
+  def update_password
+    @user = current_user
+    if password_params[:current_password].blank?
+        @user.errors.add(:current_password, :blank)
+        return render :edit_password, status: :unprocessable_entity
+    end
+
+    if password_params[:password].blank?
+        @user.errors.add(:password, :blank)
+        return render :edit_password, status: :unprocessable_entity
+    end
+
+    if @user.update_with_password(password_params)
+      bypass_sign_in(@user)
+      redirect_to account_setting_path, notice: t("defaults.flash_message.account_setting.updated")
+    else
+      flash.now[:alert] = t("defaults.flash_message.account_setting.not_updated")
+      render :edit_password, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def email_params
     params.require(:user).permit(:email)
+  end
+
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
   end
 end

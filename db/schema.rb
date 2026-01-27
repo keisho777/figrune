b8f0c1c3fabf45030622b9e1d92d52341d3cdf2d
keisho777 <komatsu.kei0130@gmail.com>
2026-01-27 23:33:51 +0900

    feat: Figureテーブルにtotal_priceカラムを追加
    合計金額のソートに使用するため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_25_063739) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_27_142723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_25_063739) do
     t.bigint "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_price", default: 0, null: false
     t.index ["manufacturer_id"], name: "index_figures_on_manufacturer_id"
     t.index ["shop_id"], name: "index_figures_on_shop_id"
     t.index ["user_id"], name: "index_figures_on_user_id"

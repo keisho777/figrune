c6b7586fabdfaecf7663ad8acd82ca96bec1cdc3
keisho777 <komatsu.kei0130@gmail.com>
2026-01-28 19:39:08 +0900

    Revert "feat: 支払い済みの場合、一覧画面の合計金額に取り消し線を表示"

--- app/helpers/figures_helper.rb
+++ app/helpers/figures_helper.rb
@@ -1,8 +1,2 @@
 module FiguresHelper
-  def display_total_price(figure)
-    total_price = number_to_currency(figure.total_price, unit: "¥", format: "%u%n")
-    css_class = figure.paid? ? "text-xl font-bold line-through" : "text-xl font-bold"
-
-    content_tag(:span, total_price, class: css_class)
-  end
 end

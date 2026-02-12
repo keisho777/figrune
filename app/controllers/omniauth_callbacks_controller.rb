7f1a1659b84bff36d9c4019da2262002a8c595de
keisho777 <komatsu.kei0130@gmail.com>
2026-02-12 11:32:49 +0900

    refactor: ログイン、新規登録の処理を分割
    可読性向上のため

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,30 +9,40 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @omniauth = request.env["omniauth.auth"]
 
     # 認証情報の確認
-    return redirect_to new_user_session_path, alert: "認証に失敗しました" if @omniauth.blank?
+    return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.invalid_credential") if @omniauth.blank?
 
-    # 認証情報の検索・紐付け
+    # 認証情報の検索
     @authentication = Authentication.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
 
-    # すでに連携済みならログイン、新規登録ならUserとAuthenticationを登録
+    # 認証情報にuser_idがあればログイン、なければ新規登録
     if @authentication.user.blank?
-      # emailの取得ができなかった場合はダミーのメールアドレスを使用
-      email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
-      user = User.new(email: email, password: Devise.friendly_token[0, 20], has_email: @omniauth["info"]["email"].present?, has_password: false)
-
-      # 認証メールのスキップ
-      user.skip_confirmation!
-      unless user.save
-        return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.email_taken")
-      end
-      @authentication.user = user
-      unless @authentication.save
-        return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.auth_save_failure")
-      end
+      register_external_user(@omniauth, @authentication)
+    else
+      authenticate_external_user(@omniauth, @authentication)
     end
+  end
 
-    # ログイン処理
-    sign_in(:user, @authentication.user)
+  # 新規登録用
+  def register_external_user(omniauth, authentication)
+    email = omniauth["info"]["email"] || "#{omniauth["uid"]}-#{omniauth["provider"]}@example.com"
+    user = User.new(email: email, password: Devise.friendly_token[0, 20], has_email: @omniauth["info"]["email"].present?, has_password: false)
+
+    # 認証メールのスキップ
+    user.skip_confirmation!
+    unless user.save
+      return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.email_taken")
+    end
+    authentication.user = user
+    unless authentication.save
+      return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.auth_save_failure")
+    end
+    sign_in(:user, authentication.user)
+    redirect_to home_path, notice: t("devise.registrations.signed_up")
+  end
+
+  # ログイン用
+  def authenticate_external_user(omniauth, authentication)
+    sign_in(:user, authentication.user)
     redirect_to home_path, notice: t("devise.sessions.signed_in")
   end
 end

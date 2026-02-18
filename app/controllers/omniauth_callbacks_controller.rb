c2456b47026f818ba03a7b831b3f02e821bb7460
keisho777 <komatsu.kei0130@gmail.com>
2026-02-18 20:13:05 +0900

    fix: SNSアカウントでの登録時にトランザクション処理を実装
    以前の実装だと登録時にエラーが発生すると詰む可能性があったため

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -25,19 +25,28 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # 新規登録用
   def register_external_user(omniauth, authentication)
     email = omniauth["info"]["email"] || "#{omniauth["uid"]}-#{omniauth["provider"]}@example.com"
-    user = User.new(email: email, password: Devise.friendly_token[0, 20], has_email: @omniauth["info"]["email"].present?, has_password: false)
+    user = User.new(email: email, password: Devise.friendly_token[0, 20], has_email: omniauth["info"]["email"].present?, has_password: false)
 
     # 認証メールのスキップ
     user.skip_confirmation!
-    unless user.save
-      return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.email_taken")
-    end
-    authentication.user = user
-    unless authentication.save
-      return redirect_to new_user_session_path, alert: t("defaults.flash_message.omniauth_callback.auth_save_failure")
+    ActiveRecord::Base.transaction do
+      # ユーザー登録
+      user.save!
+      # user_id、provider、uidの登録
+      authentication.user = user
+      authentication.save!
     end
     sign_in(:user, authentication.user)
     redirect_to home_path, notice: t("devise.registrations.signed_up")
+
+    # 登録時、すでに同じメールアドレスが登録されている場合、ここでキャッチされる
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.error(I18n.t("defaults.omniauth_error_log", error_class: e.class, error_message: e.message))
+      redirect_to new_user_registration_path, alert: e.message
+    # 予期せぬエラー
+    rescue => e
+      Rails.logger.error(I18n.t("defaults.omniauth_unexpected_error_log", error_class: e.class, error_message: e.message))
+      redirect_to new_user_registration_path, alert: t("defaults.unexpected_error")
   end
 
   # ログイン用
